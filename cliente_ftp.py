ec06587dee207cc3ac3ca288581a72852ebba821
Rodrigo de León <rodrigodeleonv@gmail.com>
2020-05-06 14:25:18 -0600

    Mikrotik API for python3 test example

--- cliente_ftp.py
+++ cliente_ftp.py
@@ -1,50 +1,40 @@
-# -*- coding: utf-8 -*-
+"""
+Ejemplo conceptual
+FTP Client
+"""
 
-#
-# Ejemplo conceptual
-# FTP Client
-#
-
+import sys
 import socket
 from ftplib import FTP, error_perm
 
+
 # Autenticación
-ROUTER_username = ''
-ROUTER_password = ''
-host = ''	# IP addr.
-SOCKET_TIMEOUT = 3		# Segundos
+username = ''
+password = ''
+HOST = ''
 PORT_FTP = 21
 
 # FTP
-# NLST:	Returns a list of file names in a specified directory.
+# NLST:	Returns a list of f names in a specified directory.
 # https://en.wikipedia.org/wiki/List_of_FTP_commands
 
+
 try:
-	ftp = FTP(host, timeout=6)
-except socket.error as e:
-	print(e)
+    ftp = FTP(timeout=6)
+    ftp.connect(HOST, PORT_FTP)
+    ftp.login(username, password)
+    files = ftp.nlst('backups')  # Files inside backups folder
+    ftp.quit()
+except error_perm as e:
+    e = str(e)
+    if '530' in e:
+        sys.exit('¡Las credenciales son incorrectas!')
+    elif '550' in e:
+        sys.exit('No existe el directorio en el servidor')
+    else:
+        raise
+except socket.timeout:
+    sys.exit('Timeout!')
 
-try:                
-	ftp.login(ROUTER_username, ROUTER_password)
-	listar_archivos = ftp.nlst()
-	for file in listar_archivos:
-		print('* '+file)
-except error_perm, resp:
-	resp = str(resp)
-	if '530' in resp: # FTP Login Incorrecto
-		print('¡Las credenciales son incorrectas!')
-	else:
-		print(resp)
-		# raise
-except AttributeError as e:
-	print(e, host)
-except socket.timeout as e:
-	print('{} ftp.login timeout'.format(e), host)
-
-# Cerrando conexión FTP
-try:
-	ftp.quit()
-except Exception as e:
-	pass
-print('Conexion FTP cerrada')
-
+for f in files:
+    print('- ' + f)
--- hash_simple.py
+++ hash_simple.py
@@ -1,26 +1,23 @@
-# -*- coding: utf-8 -*-
+"""
+Hash files
+encode() 		: convertir string a bytes.
+hexdigest() 	: retorna la inforamción en formato hexadecimal.
+"""
 
-# encode() 		: convertir string a bytes.
-# hexdigest() 	: retorna la inforamción en formato hexadecimal.
-
-import sys
 import hashlib
+
 
 text = 'Texto de prueba'
 result = hashlib.sha1(text.encode())
 print('{} HASH={}'.format(text, result.hexdigest()))
 
 
-## Hash file
+# Hash file
+content = None
+with open('hash_this_file.txt', 'rb') as file:
+    content = file.read()
 
-try:
-	content = None
-	with open('hash_this_file.txt', 'rb') as file:
-		content = file.read()
-except Exception as e:
-	print('Ha ocurrido un error al abrir el archivo: {}'.format(e))
-	sys.exit(0)
 
-## Equivalente: sha1sum <filename>
+# Equivalente: sha1sum <filename>
 result = hashlib.sha1(content)
 print('HASH del archivo: {}'.format(result.hexdigest()))
